591429d2c667fc9f4db6e734622f79d5d70c88ac
emily <emily@cute.invalid>
2026-02-25 23:48:03 -0800

    ui: convert card to component css

--- ui/card.go
+++ ui/card.go
@@ -6,21 +6,15 @@ import (
 )
 
 func Card(attrs ...attr.Node) html.Element {
-	sizeClass := "p-3 rounded-lg [--inset-padding:-12px] [--inset-rounded:8px]"
-	switch getOption[sizeOption](attrs, 1) {
-	case 2:
-		sizeClass = "p-4 rounded-lg [--inset-padding:-16px] [--inset-rounded:8px]"
-	case 3:
-		sizeClass = "p-6 rounded-xl [--inset-padding:-24px] [--inset-rounded:12px]"
-	}
-
 	return html.Div(
-		Class(sizeClass),
-		Class(`
-			bg-gray-1/70
-			inset-ring
-			inset-ring-gray-6
-		`),
+		attr.Class("a$card"),
+		attr.Class(cardSizeClasses[getOption[sizeOption](attrs, 1)]),
 		group(attrs...),
 	)
+}
+
+var cardSizeClasses = map[sizeOption]string{
+	size1: "a$card+size-1",
+	size2: "a$card+size-2",
+	size3: "a$card+size-3",
 }
