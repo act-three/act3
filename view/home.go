f8f5984125fe28e532aa578ee0fc8df42d6d3306
emily <emily@cute.invalid>
2026-03-03 00:13:08 -0800

    view: remove unused view MediaListSeries

--- view/home.go
+++ view/home.go
@@ -46,3 +46,26 @@ func Home(a []*model.SeriesHead) html.Node {
 
 	)
 }
+
+func seriesPosterLink(sr *model.SeriesHead) html.Node {
+	return html.Div(Class(`
+		aspect-2/3
+		w-[187px]
+		relative
+		hover:after:content-[""]
+		hover:after:absolute
+		hover:after:inset-0
+		hover:after:bg-black/40
+		hover:after:pointer-events-none
+		`))(
+		html.A(
+			Class("block w-full h-full"),
+			attr.Href(sr.PlayURL()),
+		)(
+			html.Img(
+				Class("w-full h-full object-cover"),
+				attr.Src(sr.TVmazeImageURL()),
+			),
+		),
+	)
+}
