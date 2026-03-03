49f4b477716c75a2be490107f5e13f86dd3de3fa
emily <emily@cute.invalid>
2026-03-02 20:42:21 -0800

    ui: fill in missing grid column layout utilities

--- ui/grid.go
+++ ui/grid.go
@@ -1,27 +1,42 @@
 package ui
 
 import (
+	"fmt"
+
 	"ily.dev/act3/html"
 	"ily.dev/act3/html/attr"
 )
 
-func Grid12(attrs ...attr.Node) html.Element {
+func grid(n int, attrs []attr.Node) html.Element {
 	return Box(
-		Class("u-grid-12"),
+		Class("u-grid"),
+		attr.Style(fmt.Sprintf("--cols:%d", n)),
 		group(attrs...),
 	)
 }
 
-func Grid9(attrs ...attr.Node) html.Element {
-	return Box(
-		Class("u-grid-9"),
-		group(attrs...),
-	)
-}
+func Grid2(attrs ...attr.Node) html.Element  { return grid(2, attrs) }
+func Grid3(attrs ...attr.Node) html.Element  { return grid(3, attrs) }
+func Grid4(attrs ...attr.Node) html.Element  { return grid(4, attrs) }
+func Grid5(attrs ...attr.Node) html.Element  { return grid(5, attrs) }
+func Grid6(attrs ...attr.Node) html.Element  { return grid(6, attrs) }
+func Grid7(attrs ...attr.Node) html.Element  { return grid(7, attrs) }
+func Grid8(attrs ...attr.Node) html.Element  { return grid(8, attrs) }
+func Grid9(attrs ...attr.Node) html.Element  { return grid(9, attrs) }
+func Grid10(attrs ...attr.Node) html.Element { return grid(10, attrs) }
+func Grid11(attrs ...attr.Node) html.Element { return grid(11, attrs) }
+func Grid12(attrs ...attr.Node) html.Element { return grid(12, attrs) }
 
-func Grid8(attrs ...attr.Node) html.Element {
-	return Box(
-		Class("u-grid-8"),
-		group(attrs...),
-	)
-}
+var (
+	ColSpan2  = Class("u-col-span-2")
+	ColSpan3  = Class("u-col-span-3")
+	ColSpan4  = Class("u-col-span-4")
+	ColSpan5  = Class("u-col-span-5")
+	ColSpan6  = Class("u-col-span-6")
+	ColSpan7  = Class("u-col-span-7")
+	ColSpan8  = Class("u-col-span-8")
+	ColSpan9  = Class("u-col-span-9")
+	ColSpan10 = Class("u-col-span-10")
+	ColSpan11 = Class("u-col-span-11")
+	ColSpan12 = Class("u-col-span-12")
+)
