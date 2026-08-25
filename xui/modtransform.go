ddaa75dc601adee41cc10ab6a09a8ccc2acaaf8d
emily <em@cute.invalid>
2026-08-25 01:38:03 -0700

    xui: refactor state-scoped transform modifiers

---
+++ xui/modtransform.go
@@ -0,0 +1,34 @@
+package ui
+
+// A modTransformState modifies the environment given to a node,
+// associated with a set of browser states.
+type modTransformState func(environment, State) environment
+
+func (m modTransformState) withState(s State) modifier {
+	return modTransform(func(env environment) environment {
+		return m(env, s)
+	})
+}
+
+// A modTransform modifies the environment given to a node.
+type modTransform func(environment) environment
+
+func (m modTransform) modify(n node) node {
+	return nodeTransform{m, n}
+}
+
+// BorderShape sets the shape of a view's border.
+func BorderShape(s Shape) Modifier {
+	return modTransformState(func(env environment, state State) environment {
+		env.shape = append(env.shape, term[Shape]{state, s})
+		return env
+	})
+}
+
+// Opacity sets a view's opacity to x, from 0 (transparent) to 1 (opaque).
+func Opacity(x float64) Modifier {
+	return modTransformState(func(env environment, s State) environment {
+		env.opacity = append(env.opacity, term[float64]{s, x})
+		return env
+	})
+}
